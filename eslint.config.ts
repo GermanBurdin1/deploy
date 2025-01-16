d1a57a820d9335a6ee34443da7fae8fe7ff56143
German <125369843+GermanBurdin1@users.noreply.github.com>
2025-01-16 14:36:17 +0100

    upd

---
+++ eslint.config.ts
@@ -0,0 +1,22 @@
+const parser = require('@typescript-eslint/parser');
+const plugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'], // Указываем файлы TypeScript
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
